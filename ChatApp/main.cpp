663b3fa983575fe9ccfef560e25eb520f91eb51f
gino <gino@gino-HP-Pavilion-15-Notebook-PC>
2016-04-07 21:06:56 +0200

    add space support
    actual sentences are now possible thanks to the invention of the space

--- ChatApp/main.cpp
+++ ChatApp/main.cpp
@@ -17,18 +17,20 @@ int main() {
 	std::string DestinationIP;
 	std::string MyIP = getIP();
 	std::string Message;
-	multisend(14000, "228.1.2.3", MyIP, "HALLO");
+	//multisend(14000, "228.1.2.3", MyIP, "HALLO");
 	//std::thread receiver(multireceive, 14000, "228.1.2.3", MyIP,std::ref(q));
 	//tcpsend(55056, "192.168.5.2", "Holabola");
-	std::thread receiver(tcpreceive, 55056, std::ref(q));
+	std::thread receiver(tcpreceive, 14000, std::ref(q));
 	std::cout << "With who will you chat this session?" << std::endl;
 	std::cin >> DestinationIP;
 	while (1) {
-		std::cin >> Message;
+		std::cout << "say something!" << std::endl;
+		std::getline(std::cin, Message);
 		std::cout << "You: " << Message << std::endl;
 		sendMessage(DestinationIP, Message);
-		std::string message = q.pop();
-		std::cout << message << std::endl;
+		//std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		//std::string message = q.pop();
+		//std::cout << message << std::endl;
 	}
 
 }
