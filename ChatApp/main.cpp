f27b5d267009d0152ef3c80bed1a111d999fa2fa
gino <gino@gino-HP-Pavilion-15-Notebook-PC>
2016-04-07 15:59:21 +0200

    main is fixes

--- ChatApp/main.cpp
+++ ChatApp/main.cpp
@@ -13,26 +13,14 @@ BlockingQueue<std::string> q;
 
 int main() {
 
-<<<<<<< HEAD
-	//std::thread receiver(multirecieve, 14000, "228.1.2.3", "192.168.5.2",std::ref(q));
-	tcpsend(4242, "192.168.5.2", "192,168.5.1", "Holabola TCP!");
-	//multisend(55056, "228.1.2.3", "192,168.5.2", "Holabola");
-	//while (1) {
-
-		//std::string message = q.pop();
-		//std::cout << "main thread sees something! It sees:" << std::endl;
-		//std::cout << message << std::endl;
-
-		//}
-=======
 	multisend(14000, "228.1.2.3", "192.168.5.1", "HALLO");
-	std::thread receiver(multirecieve, 14000, "228.1.2.3", "192.168.5.1",std::ref(q));
+	std::thread receiver(multirecieve, 14000, "228.1.2.3", "192.168.5.1",
+			std::ref(q));
 	tcpsend(55056, "192.168.5.1", "192,168.5.2", "Holabola");
 	while (1) {
 		std::string message = q.pop();
 		std::cout << "main thread sees something! It sees:" << std::endl;
 		std::cout << message << std::endl;
-		}
->>>>>>> branch 'master' of https://github.com/Rodelbing/IntegratedProject.git
-
 	}
+
+}
