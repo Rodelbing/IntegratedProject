d14da7a1811b491a1135935a4340f07dfc3342de
nahuel <nahuel@Nahuel>
2016-04-07 15:27:22 +0200

    tcprec

--- ChatApp/main.cpp
+++ ChatApp/main.cpp
@@ -5,20 +5,22 @@
 #include "network/multirecieve.h"
 #include "network/multisend.h"
 #include "network/tcpsend.h"
+#include "network/tcpreceive.h"
 #include "lib/BlockingQueue.h"
 
 BlockingQueue<std::string> q;
 
 int main() {
 	//std::thread receiver(multirecieve, 14000, "228.1.2.3", "192.168.5.2",std::ref(q));
-	tcpsend(55056, "192.168.5.1", "192,168.5.2", "Holabola TCP!");
+	//tcpsend(55056, "192.168.5.1", "192,168.5.2", "Holabola TCP!");
+	tcpreceive(4242, "192.168.5.2",  std::ref(q));
 	//multisend(55056, "228.1.2.3", "192,168.5.2", "Holabola");
-	//while (1) {
+	while (1) {
 
-		//std::string message = q.pop();
-		//std::cout << "main thread sees something! It sees:" << std::endl;
-		//std::cout << message << std::endl;
+		std::string message = q.pop();
+		std::cout << "main thread sees something! It sees:" << std::endl;
+		std::cout << message << std::endl;
 
-		//}
+		}
 
 	}
---
+++ ChatApp/network/tcpreceive.cpp
@@ -0,0 +1,78 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Apr 7, 2016
+ *      Author: gino
+ */
+
+#include <arpa/inet.h>
+#include <iostream>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include "../lib/BlockingQueue.h"
+#define BUFSIZE 2048
+
+
+int makeTCPsocket(int PORT, std::string ip){
+	int pack;
+
+	if ((pack = socket(AF_INET, SOCK_STREAM,0)) < 0){
+		perror("cannot create socket!");
+		return 0;
+	}
+	else{
+		std::cout << "created socket: descriptor = " << pack << std::endl;
+	}
+
+	struct sockaddr_in myaddr;
+	memset((char *)&myaddr, 0, sizeof(myaddr));
+	myaddr.sin_family = AF_INET;
+	myaddr.sin_addr.s_addr = htonl(0);
+	myaddr.sin_port = htons(PORT);
+
+	if (bind(pack, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0){
+		perror ("bind failed");
+		return 0;
+	}
+	else{
+		std::cout << "succesfull, port = " << ntohs(myaddr.sin_port) << std::endl;
+	}
+
+
+ return pack;
+
+}
+
+int tcpreceive(int PORT, std::string ip,  BlockingQueue<std::string> &q){
+
+	int pack = makeTCPsocket(PORT, ip);
+	ssize_t recvlen;
+	char buf[BUFSIZE];
+	if((listen(pack, 3)) < 0){
+		perror("I won't receive!");
+	}
+	recvlen = 0;
+	struct sockaddr_in peer_address;
+	socklen_t peer_address_len = sizeof(struct sockaddr_storage);
+	while(true){
+
+			if ((accept(pack, (struct sockaddr *) &peer_address, &peer_address_len)) < 0) {
+				perror("cannot accept!");
+			}
+
+			recvlen = recv(pack,buf, BUFSIZE,0);
+			q.push(std::string(buf, recvlen));
+			std::cout << buf << std::endl;
+			}
+
+
+		exit(0);
+}
+
