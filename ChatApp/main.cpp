ea6de56778742145f1307b8f7c2430d8e714d073
gino <gino@gino-HP-Pavilion-15-Notebook-PC>
2016-04-07 16:11:17 +0200

    get ip, makes life easier (sometimes...)

--- ChatApp/main.cpp
+++ ChatApp/main.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <unistd.h>
 
+#include "network/getIP.h"
 #include "network/multisend.h"
 #include "network/multirecieve.h"
 #include "network/multisend.h"
@@ -13,10 +14,11 @@ BlockingQueue<std::string> q;
 
 int main() {
 
-	multisend(14000, "228.1.2.3", "192.168.5.1", "HALLO");
-	std::thread receiver(multirecieve, 14000, "228.1.2.3", "192.168.5.1",
-			std::ref(q));
-	tcpsend(55056, "192.168.5.1", "192,168.5.2", "Holabola");
+	std::string MyIP = getIP();
+
+	multisend(14000, "228.1.2.3", MyIP, "HALLO");
+	std::thread receiver(multirecieve, 14000, "228.1.2.3", MyIP,std::ref(q));
+	tcpsend(55056, MyIP, "192,168.5.2", "Holabola");
 	while (1) {
 		std::string message = q.pop();
 		std::cout << "main thread sees something! It sees:" << std::endl;
