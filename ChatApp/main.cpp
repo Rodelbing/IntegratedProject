be908392c059f351e290da231b76c39282f9cdda
nahuel <nahuel@Nahuel>
2016-04-07 15:52:50 +0200

    +1

--- ChatApp/main.cpp
+++ ChatApp/main.cpp
@@ -12,7 +12,7 @@
 BlockingQueue<std::string> q;
 
 int main() {
-<<<<<<< HEAD
+
 	multisend(14000, "228.1.2.3", "192.168.5.1", "HALLO");
 	std::thread receiver(multirecieve, 14000, "228.1.2.3", "192.168.5.1",std::ref(q));
 	tcpsend(55056, "192.168.5.1", "192,168.5.2", "Holabola");
@@ -21,22 +21,5 @@ int main() {
 		std::cout << "main thread sees something! It sees:" << std::endl;
 		std::cout << message << std::endl;
 		}
-=======
-	//std::thread receiver(multirecieve, 14000, "228.1.2.3", "192.168.5.2",std::ref(q));
-	//tcpsend(55056, "192.168.5.1", "192,168.5.2", "Holabola TCP!");
-	tcpreceive(4242, "192.168.5.2",  std::ref(q));
-	//multisend(55056, "228.1.2.3", "192,168.5.2", "Holabola");
-	while (1) {
-
-		std::string message = q.pop();
-		std::cout << "main thread sees something! It sees:" << std::endl;
-		std::cout << message << std::endl;
-
-<<<<<<< HEAD
-		}
-=======
-		//}
->>>>>>> branch 'master' of https://github.com/Rodelbing/IntegratedProject.git
->>>>>>> branch 'master' of https://github.com/Rodelbing/IntegratedProject
 
 	}
