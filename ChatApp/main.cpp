46727376572dea574f9826940bc1aae899a682f0
nahuel <nahuel@Nahuel>
2016-04-08 11:57:18 +0200

    Main zonder vectorrouting

--- ChatApp/main.cpp
+++ ChatApp/main.cpp
@@ -1,3 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<X11/X.h>
+#include<X11/Xlib.h>
+#include<GL/gl.h>
+#include<GL/glx.h>
+#include<GL/glu.h>
 #include <thread>
 #include <cstring>
 #include <unistd.h>
@@ -11,15 +18,34 @@
 #include "lib/BlockingQueue.h"
 #include "network/multireceive.h"
 #include "network/sendMessage.h"
-#include "network/distancevector.h"
 
-static BlockingQueue<std::string> q;
+
+BlockingQueue<std::string> q;
+
+struct tableEntry{
+		string dest;
+		string via;
+	};
 
 int main() {
 	std::string DestinationIP;
 	std::string MyIP = getIP();
 	std::string Message;
 	vector<tableEntry> fwdTable;
-	std::thread routing(start, &fwdTable);
+	//multisend(14000, "228.1.2.3", MyIP, "HALLO");
+	//tcpsend(55056, "192.168.5.2", "Holabola");
+	std::thread receiver(tcpreceive, 14000, std::ref(q));
+	std::cout << "With who will you chat this session?" << std::endl;
+	std::cin >> DestinationIP;
+	std::cout << "Say something! (or I'm giving up on you.)" << std::endl;
+	while (1) {
+
+		std::getline(std::cin, Message);
+		std::cout << "You: " << Message << std::endl;
+		sendMessage(DestinationIP, DestinationIP, Message);
+		//std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		//std::string message = q.pop();
+		//std::cout << message << std::endl;
+	}
 
 }
