bb37d8690d8cef0a287144ce58657f9f6d5da701
nahuel <nahuel@Nahuel>
2016-04-08 11:05:23 +0200

    TCPadd

--- ChatApp/network/analyzeTCPpacket.cpp
+++ ChatApp/network/analyzeTCPpacket.cpp
@@ -1,4 +1,5 @@
 #include "getIP.h"
+#include "sendMessage.h"
 #include <iostream>
 int analyzeTCP(std::string Packet){
 
@@ -9,17 +10,19 @@ int analyzeTCP(std::string Packet){
 
 	while ((Pos = Packet.find(Delimiter)) != std::string::npos) {
 	    SplitMessage[Counter] = Packet.substr(0, Pos);
-	    std::cout << SplitMessage[Counter] << std::endl;
+	    //std::cout << SplitMessage[Counter] << std::endl;
 	    Packet.erase(0, Pos + Delimiter.length());
 	    Counter++;
 	}
 	SplitMessage[Counter] = Packet;
-	std::cout << Packet << std::endl;
+	//std::cout << SplitMessage[Counter] << std::endl;
 
 if(SplitMessage[0] == getIP()){		// If this message is directed to me:
-	std::cout << SplitMessage[1] << ": " << SplitMessage[3] << std::endl;
+	if(SplitMessage[2] == "MSG"){
+		std::cout << SplitMessage[1] << ": " << SplitMessage[3] << std::endl;
+	}
 }else{								// Elsewise forward it
-
+	sendMessage(SplitMessage[0], SplitMessage[0], SplitMessage[3]);
 }
 
 	return 0;
