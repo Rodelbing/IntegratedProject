bc083c9ad21d08b2936722589418c703230c4ddd
gino <gino@gino-HP-Pavilion-15-Notebook-PC>
2016-04-08 12:32:31 +0200

    bla

--- ChatApp/main.cpp
+++ ChatApp/main.cpp
@@ -1,10 +1,3 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<X11/X.h>
-#include<X11/Xlib.h>
-#include<GL/gl.h>
-#include<GL/glx.h>
-#include<GL/glu.h>
 #include <thread>
 #include <cstring>
 #include <unistd.h>
@@ -18,42 +11,20 @@
 #include "lib/BlockingQueue.h"
 #include "network/multireceive.h"
 #include "network/sendMessage.h"
+#include "network/distancevector.h"
 
-
-BlockingQueue<std::string> q;
-
-struct tableEntry{
-		string dest;
-		string via;
-	};
+static BlockingQueue<std::string> q;
 
 int main() {
 	std::string DestinationIP;
 	std::string MyIP = getIP();
 	std::string Message;
 	vector<tableEntry> fwdTable;
-<<<<<<< HEAD
 	std::thread routing(start, &fwdTable);
 	//std::thread test(multireceive ,14000, "228.1.2.3", MyIP, std::ref(q));
 
 	while(1){
 
-=======
-	//multisend(14000, "228.1.2.3", MyIP, "HALLO");
-	//tcpsend(55056, "192.168.5.2", "Holabola");
-	std::thread receiver(tcpreceive, 14000, std::ref(q));
-	std::cout << "With who will you chat this session?" << std::endl;
-	std::cin >> DestinationIP;
-	std::cout << "Say something! (or I'm giving up on you.)" << std::endl;
-	while (1) {
-
-		std::getline(std::cin, Message);
-		std::cout << "You: " << Message << std::endl;
-		sendMessage(DestinationIP, DestinationIP, Message);
-		//std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		//std::string message = q.pop();
-		//std::cout << message << std::endl;
->>>>>>> branch 'master' of https://github.com/Rodelbing/IntegratedProject.git
 	}
 
 }
